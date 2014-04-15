0f83a499fd20749046a547bf767b1b627a396fbf
Andre Farzat <andrefarzat@gmail.com>
2014-04-15 12:07:26 -0400

    Creating the tests for karma

--- test/tests.js
+++ test/tests.js
@@ -1,151 +1,146 @@
-module( "main function" );
+describe("main function", function(){
+
+    it('Check if all necessary properties are functions', function(){
+        expect(typeof i18n).toBe('function');
+        expect(typeof i18n._).toBe('function');
+        expect(typeof i18n.pluralize).toBe('function');
+    });
 
 
+    it('Giving an object, it must create the i18n._ object', function(){
+        
+        i18n({
+            'one' : 'first value',
+            'two' : 'second value',
+            'three' : 'third value'
+        });
 
-test('Check if all necessary properties are functions', function(){
-	ok( typeof i18n === 'function', 'i18n is not a function' );
-	ok( typeof i18n._ === 'function', "i18n._ isn't a function" );
-	ok( typeof _ === 'function', "_ isn't a function" );
-	ok( typeof i18n.pluralize === 'function', "i18n.pluralize isn't a function" );
-	ok( typeof pluralize === 'function', "pluralize isn't a function" );
-});
+        expect( i18n._.one ).toBe( 'first value' );
+        expect( i18n._.two ).toBe( 'second value' );
+        expect( i18n._.three ).toBe( 'third value' );
 
-
-test('Giving an object, it must create the i18n._ object', function(){
-	
-	i18n({
-		'one' : 'first value',
-		'two' : 'second value',
-		'three' : 'third value'
-	});
-
-	ok( _.one === 'first value' );
-	ok( _.two === 'second value' );
-	ok( _.three === 'third value' );
-
-});
+    });
 
 
-test('Giving a second object, it must add the new properties and overwrite the old ones', function(){
+    it('Giving a second object, it must add the new properties and overwrite the old ones', function(){
 
-	i18n({
-		'one' : 'new first value',
-		'two' : 'new second value',
-		'three' : 'new third value',
-		'four' : 'fourth value'
-	});
+        i18n({
+            'one' : 'new first value',
+            'two' : 'new second value',
+            'three' : 'new third value',
+            'four' : 'fourth value'
+        });
 
-	ok( _.one === 'new first value' );
-	ok( _.two === 'new second value' );
-	ok( _.three === 'new third value' );
-	ok( _('four') === 'fourth value' );
+        expect( i18n._.one ).toBe( 'new first value' );
+        expect( i18n._.two ).toBe( 'new second value' );
+        expect( i18n._.three ).toBe( 'new third value' );
+        expect( i18n._('four') ).toBe( 'fourth value' );
 
-});
+    });
 
-test('Giving an array as value, it must store it', function(){
+    it('Giving an array as value, it must store it', function(){
 
-	i18n({
-		'arr1' : ['Message one'],
-		'arr2' : ['Message two', 'Message two plural'],
-		'arr3' : ['Message three Empty', 'Message three single', 'Message three multiple']
-	});
+        i18n({
+            'arr1' : ['Message one'],
+            'arr2' : ['Message two', 'Message two plural'],
+            'arr3' : ['Message three Empty', 'Message three single', 'Message three multiple']
+        });
 
-	deepEqual( _.arr1, ['Message one'] );
-	deepEqual( _.arr2, ['Message two', 'Message two plural'] );
-	deepEqual( _.arr3, ['Message three Empty', 'Message three single', 'Message three multiple'] );
+        expect( i18n._.arr1).toEqual( ['Message one'] );
+        expect( i18n._.arr2).toEqual( ['Message two', 'Message two plural'] );
+        expect( i18n._.arr3).toEqual( ['Message three Empty', 'Message three single', 'Message three multiple'] );
 
-});
+    });
 
-test("Giving a non-object argument must do nothing", function(){
+    it("Giving a non-object argument must do nothing", function(){
 
-	//Must do nothing!
-	i18n(null);
-	i18n(NaN);
-	i18n(undefined);
-	i18n([]);
+        //Must do nothing!
+        i18n(null);
+        i18n(NaN);
+        i18n(undefined);
+        i18n([]);
 
-	throws(function(){
-		i18n(function(){});
-	}, 'Function cannot pass');
+        expect(function(){
+            i18n(function(){});
+        }).toThrow();
 
-	throws(function(){
-		i18n(['one']);
-	}, 'Array cannot pass');	
+        expect(function(){
+            i18n(['one']);
+        }).toThrow();
 
-	throws(function(){
-		i18n(100);
-	}, 'Number is not acceptable.');
+        expect(function(){
+            i18n(100);
+        }).toThrow();
 
-	throws(function(){
-		i18n('string should not pass!');
-	}, 'String is not acceptable.');
+        expect(function(){
+            i18n('string should not pass!');
+        }).toThrow();
 
-});
-
-
-module( 'i18n._ function' );
+    });
 
 
-test('Testing with one argument', function(){
+    describe('i18n._ function', function(){
 
-	equal( _('one'), 'new first value');
-	equal( _('two'), 'new second value');
-	equal( _('four'), 'fourth value');
+        it('Testing with one argument', function(){
 
-});
+            expect( i18n._('one') ).toEqual( 'new first value' );
+            expect( i18n._('two') ).toEqual( 'new second value' );
+            expect( i18n._('four') ).toEqual( 'fourth value' );
 
-test('Giving an inexistent key, it must return the given key', function(){
+        });
 
-	var txt = 'my amazing key';
+        it('Giving an inexistent key, it must return the given key', function(){
 
-	equal( _(txt), txt );
+            var txt = 'my amazing key';
 
-});
+            expect( i18n._(txt) ).toEqual( txt );
 
-test('Testing the pluralize', function(){
+        });
 
-	var obj = {
-		'0' : 'Nothing',
-		'1' : 'One thing',
-		'2' : 'Two things',
-		'7' : 'Seven things',
-		'one' : 'Someone is seeing',
-		'other' : i18n.wildcard + ' things!'
-	};
+        it('Testing the pluralize', function(){
 
-	equal( obj[0], pluralize( obj, 0 ) );
-	equal( obj[1], pluralize( obj, 1 ) );
-	equal( obj[2], pluralize( obj, 2 ) );
-	equal( obj[7], pluralize( obj, 7 ) );
+            var obj = {
+                '0' : 'Nothing',
+                '1' : 'One thing',
+                '2' : 'Two things',
+                '7' : 'Seven things',
+                'one' : 'Someone is seeing',
+                'other' : i18n.wildcard + ' things!'
+            };
 
-	//offset
-	equal( obj[7], pluralize( obj, 9, 2 ) );
-	equal( obj[2], pluralize( obj, 4, 2 ) );
-	
-	//One
-	equal( obj['one'], pluralize( obj, 4, 3 ) );
-	equal( obj['one'], pluralize( obj, 'one' ) );
+            expect( obj[0] ).toEqual( i18n.pluralize( obj, 0 ) );
+            expect( obj[1] ).toEqual( i18n.pluralize( obj, 1 ) );
+            expect( obj[2] ).toEqual( i18n.pluralize( obj, 2 ) );
+            expect( obj[7] ).toEqual( i18n.pluralize( obj, 7 ) );
 
-	//others
-	var txt = obj['other'].replace( i18n.wildcard, -1 );
-	equal( txt, pluralize( obj, -1 ), '-1' );
+            //offset
+            expect( obj[7] ).toEqual( i18n.pluralize( obj, 9, 2 ) );
+            expect( obj[2] ).toEqual( i18n.pluralize( obj, 4, 2 ) );
+            
+            //One
+            expect( obj['one'] ).toEqual( i18n.pluralize( obj, 4, 3 ) );
+            expect( obj['one'] ).toEqual( i18n.pluralize( obj, 'one' ) );
 
-	txt = obj['other'].replace( i18n.wildcard, 8 );
-	equal( txt, pluralize( obj, 10, 2 ) );
+            //others
+            var txt = obj['other'].replace( i18n.wildcard, -1 );
+            expect( txt ).toEqual( i18n.pluralize( obj, -1 ), '-1' );
 
-	txt = obj['other'].replace( i18n.wildcard, 100 );
-	equal( txt, pluralize( obj, 100 ) );
+            txt = obj['other'].replace( i18n.wildcard, 8 );
+            expect( txt ).toEqual( i18n.pluralize( obj, 10, 2 ) );
 
-	//Not numbers will always return zero
-	equal( obj[0], pluralize( obj, 'not number' ) );
-	equal( obj[0], pluralize( obj, NaN ) );
-	equal( obj[0], pluralize( obj, '' ) );
-	equal( obj[0], pluralize( obj, false ) );
-	equal( obj[0], pluralize( obj, true ) );
-	equal( obj[0], pluralize( obj, Infinity ) );
-	equal( obj[0], pluralize( obj, {} ) );
+            txt = obj['other'].replace( i18n.wildcard, 100 );
+            expect( txt ).toEqual( i18n.pluralize( obj, 100 ) );
 
-	//offset as a negative number
-	throws( obj[7], pluralize( obj, 9, -2 ), 'Must throw an exception!' );
+            //Not numbers will always return zero
+            expect( obj[0]).toEqual( i18n.pluralize( obj, 'not number' ) );
+            expect( obj[0]).toEqual( i18n.pluralize( obj, NaN ) );
+            expect( obj[0]).toEqual( i18n.pluralize( obj, '' ) );
+            expect( obj[0]).toEqual( i18n.pluralize( obj, false ) );
+            expect( obj[0]).toEqual( i18n.pluralize( obj, true ) );
+            expect( obj[0]).toEqual( i18n.pluralize( obj, Infinity ) );
+            expect( obj[0]).toEqual( i18n.pluralize( obj, {} ) );
+        });
+
+    });
 
 });
